6302f74c2174dcbe6b2aa8daf35702a2269aa099
Pablo Orviz <orviz@ifca.unican.es>
2016-06-28 09:31:29 +0000

    setup logging

--- generate_reports.py
+++ generate_reports.py
@@ -15,34 +15,13 @@ import logging.config
 
 from api import jenkins
 
+
+with open("etc/logging.yaml") as f:
+    d = yaml.load(f)
+    d.setdefault("version", 1)
+    logging.config.dictConfig(d)
 logger = logging.getLogger(__name__)
-# load config from file
-# logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
-# or, for dictConfig
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': False,  # this fixes the problem
-
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'default': {
-            'level':'INFO',
-            'class':'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['default'],
-            'level': 'INFO',
-            'propagate': True
-        }
-    }
-})
-
+logging.getLogger("requests").setLevel(logging.WARNING)
 
 
 def parse_args():
@@ -102,7 +81,7 @@ def add_jenkins_job(specs, job_type):
     elif "url_external" in specs[job_type].keys():
         if specs[job_type]["url_external"]:
             specs[job_type]["jobs"] = specs[job_type]["url_external"]
-            logger.info("External URL for %s found: %s"
+            logger.debug("External URL for %s found: %s"
                         % (job_type, specs[job_type]["jobs"]))
 
 
@@ -112,7 +91,7 @@ def main(fname, specdir, output=None, code_style=None):
         spec_yaml_files = glob.glob(os.path.join(specdir, "*.yaml"))
     elif os.path.isfile(specdir):
         spec_yaml_files = [specdir]
-    
+
     if not os.path.exists(output):
         os.makedirs(output)
 
@@ -133,16 +112,16 @@ def main(fname, specdir, output=None, code_style=None):
 
         # specs - code_style
         if specs["code_style"]["jenkins_job"]:
-            logger.info("Jenkins job/s for code style found.")
+            logger.debug("Jenkins job/s for code style found.")
             specs["code_style"]["jobs"] = {}
             for j in specs["code_style"]["jenkins_job"]:
                 specs["code_style"]["jobs"][j] = {}
                 specs["code_style"]["jobs"][j]["job_url"] = jenkins.get_last_job_url(j)
                 specs["code_style"]["jobs"][j]["job_name"] = j
         else:
-            logger.info("No Jenkins job found for code style.")
+            logger.debug("No Jenkins job found for code style.")
         if specs["code_style"]["standard"]:
-            logger.info("Code style standard/s adhered: %s"
+            logger.debug("Code style standard/s adhered: %s"
                          % ','.join(specs["code_style"]["standard"]))
             try:
                 for standard in specs["code_style"]["standard"]:
@@ -151,10 +130,10 @@ def main(fname, specdir, output=None, code_style=None):
                     except KeyError:
                         specs["code_style"]["data"] = {}
                     specs["code_style"]["data"][standard] = code_style[standard]
-            except KeyError, e:
+            except KeyError as e:
                 specs["code_style"]["data"] = {}
         else:
-            logger.info("No code_style standard/s defined.")
+            logger.debug("No code_style standard/s defined.")
 
         # specs - unit_test
         if specs["unit_test"]["jenkins_job"]:
@@ -165,38 +144,43 @@ def main(fname, specdir, output=None, code_style=None):
                 dest_dir=os.path.join(output, "figs"))
             specs["unit_test"]["data"] = jenkins.get_cobertura_data(
                 specs["unit_test"]["jenkins_job"])
-            logger.info("Jenkins job for unit testing found: %s"
+            logger.debug("Jenkins job for unit testing found: %s"
                          % specs["unit_test"]["job_url"])
         elif "url_external" in specs["unit_test"].keys():
             specs["unit_test"]["job_url"] = specs["unit_test"]["url_external"]
-            logger.info("External URL for unit testing found: %s"
+            logger.debug("External URL for unit testing found: %s"
                          % specs["unit_test"]["job_url"])
         else:
-            logger.info("No jenkins job defined in configuration.")
+            logger.debug("No jenkins job defined in configuration.")
 
         # specs - func_int_test
         add_jenkins_job(specs, "func_int_test")
         if specs["func_int_test"]:
-            logger.info("Functional/integration tests defined.")
+            logger.debug("Functional/integration tests defined.")
         else:
-            logger.info("No functional/integration definition found.")
+            logger.debug("No functional/integration definition found.")
 
         # specs - config_management
         specs["config_management"]["job_url"] = jenkins.get_last_job_url(
             specs["config_management"]["jenkins_job"])
         if specs["config_management"]["job_url"]:
-            logger.info("Configuration management job found: %s"
+            logger.debug("Configuration management job found: %s"
                          % specs["config_management"]["jenkins_job"])
         else:
-            logger.info("No Jenkins job for configuration management found.")
+            logger.debug("No Jenkins job for configuration management found.")
 
         # latex
+        #FIXME(orviz) these values must be CLI arguments
+        period="20-24 Jun 2016",
+        weeks=12,
+        current_week=10,
+        logger.info("Reports being generated for period '%s' (week %s out of %s)" % (period, weeks, current_week))
         template = latex_jinja_env.get_template(os.path.basename(fname))
         r = template.render(
             product=specs,
-            period="20-24 Jun 2016",
-            weeks=12,
-            current_week=10,
+            period=period,
+            weeks=weeks,
+            current_week=current_week,
         )
 
         texfile = os.path.basename(f).split('.')[0] + '.tex'
@@ -208,17 +192,20 @@ def main(fname, specdir, output=None, code_style=None):
 
     if output:
         pdfdir = os.path.join(output, "pdf")
+        logger.info("Report PDFs stored in '%s'" % pdfdir)
         if not os.path.exists(pdfdir):
             os.makedirs(pdfdir)
         for f in glob.glob(os.path.join(os.path.dirname(fname), "title_*.tex")):
             shutil.copy(f, output)
         for texfile in texfiles:
             f = os.path.join(output, texfile)
-            # dirty way of setting timeout
-            p = Popen(["pdflatex", "-output-directory=%s" % pdfdir, f], stdout=PIPE, stderr=PIPE)
+            logger.debug("Building LaTeX doc: %s" % f)
+            cmd = ["pdflatex", "-output-directory=%s" % pdfdir, f]
+            logger.debug("Running command: %s" % ' '.join(cmd))
+            p = Popen(cmd, stdout=PIPE, stderr=PIPE)
             stdout, stderr = p.communicate()
             if stderr:
-                print(stdout)
+                logger.error(stdout)
 
 
 if __name__ == "__main__":
