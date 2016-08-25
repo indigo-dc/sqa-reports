7851b9529811422e871d55359c34bbf655a50d04
Pablo Orviz <orviz@ifca.unican.es>
2016-08-25 07:35:25 +0000

    week 9

--- api/jenkins.py
+++ api/jenkins.py
@@ -19,6 +19,13 @@ requests.packages.urllib3.disable_warnings()
 def get_last_buildno(job_name):
     #j = Jenkins(JENKINS_URL, requester=Requester(username, password, baseurl=JENKINS_URL, ssl_verify=False))
     j = Jenkins(JENKINS_URL, ssl_verify=False)
+    #print "***** ", dir(j)
+    #print "***** ", [job for job in j.get_jobs()]
+    #print "***** ", dir(j.get_job('ooi-unittest'))
+    #print "***** ", j.get_job('ooi-unittest').get_last_build().buildno
+    #print "****** ", dir(j.get_job(job_name[0]))
+    #print "****** ", dir(j.get_job("ooi-unittest"))
+    #print "#################### %s\n\n" % j.get_job(job_name).get_last_build().buildno
     try:
         return j.get_job(job_name).get_last_good_build().buildno
     except jenkinsapi.custom_exceptions.NoBuildData:
@@ -47,6 +54,11 @@ def save_cobertura_graph(job_name, dest_dir):
     r = requests.get(url, verify=False)
     if r.status_code == 500:
         return None
+    elif r.status_code == 404:
+        url = '/'.join([JENKINS_URL, "job/%s/%s/jacoco/graph" % (job_name, last_build)])
+        r = requests.get(url, verify=False)
+        if r.status_code == 404:
+            return None
     fname = os.path.join(dest_dir, "graph_%s.png" % job_name)
     i = Image.open(StringIO(r.content))
     i.save(fname)
@@ -60,5 +72,10 @@ def get_cobertura_data(job_name):
         return {}
     url = '/'.join([JENKINS_URL, "job/%s/%s/cobertura/api/json?depth=2" % (job_name, last_build)])
     r = requests.get(url, verify=False)
+    if r.status_code == 404:
+        # FIXME(orviz) Implement jacoco
+        #url = '/'.join([JENKINS_URL, "job/%s/%s/jacoco/api/json?depth=2" % (job_name, last_build)])
+        #r = requests.get(url, verify=False)
+        return {}
     return json.loads(r.content)["results"]["elements"]
 
